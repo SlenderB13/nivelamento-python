f4412d5a1ddb35f77e5ab8e88a66c3c849f9856a
Lucas Silva <lguilherme8@gmail.com>
2022-07-19 20:55:31 -0300

    edit: lógica nova e menos verbosa, Exercicio 29; style: mudança de código para inglês

--- parte-1/exercicio-29.py
+++ parte-1/exercicio-29.py
@@ -1,9 +1,5 @@
-cores_1 = set(["White", "Black", "Red"])
-cores_2 = set(["Red", "Green"])
-cores_ausentes = []
+colors_1 = set(["White", "Black", "Red"])
+colors_2 = set(["Red", "Green"])
 
-for cor in cores_1:
-    if cor not in cores_2:
-        cores_ausentes.append(cor)
-
-print(cores_ausentes)
+colors = {color for color in colors_1 if color not in colors_2}
+print(colors)
