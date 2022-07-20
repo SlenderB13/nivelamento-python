c7ca44d00e22dbe3cc22175e2179a5ac9a6cd1f0
Lucas Silva <lguilherme8@gmail.com>
2022-07-20 20:13:49 -0300

    refact: variavel validada por sintaxe simplificada do set, Exercicio-29, Parte-1

--- parte-1/exercicio-29.py
+++ parte-1/exercicio-29.py
@@ -1,5 +1,5 @@
 colors_1 = set(["White", "Black", "Red"])
 colors_2 = set(["Red", "Green"])
 
-colors = {color for color in colors_1 if color not in colors_2}
+colors = set(colors_1 - colors_2)
 print(colors)
