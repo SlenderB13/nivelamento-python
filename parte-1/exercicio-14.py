1e8a909bad71cf6cb201c3e9eadcdd4fe92cf8eb
Lucas Silva <lguilherme8@gmail.com>
2022-07-20 20:08:06 -0300

    refact: variavel tem valor absoluto para evitar mostrar negativo, Exercicio-14, Parte-1

--- parte-1/exercicio-14.py
+++ parte-1/exercicio-14.py
@@ -1,7 +1,8 @@
 from datetime import date
 
-initial_date = date(2024, 9, 2)
-final_date = date(2022, 7, 18)
+initial_date = date(2022, 9, 2)
+final_date = date(2024, 7, 18)
 
-days = (initial_date - final_date).days
+days = abs((initial_date - final_date).days)
 print(f'Days: {days}')
+#TODO: DEIXAR O ABS NA VARIAVEL
