3a9ac50a549301e5705641aef5a93f4a02a1ceb8
Lucas Silva <lguilherme8@gmail.com>
2022-07-19 21:22:54 -0300

    edit: lógica nova e menos verbosa com TypeCasting, Exercicio 73; style: mudança de código para inglês

--- parte-2/exercicio-73.py
+++ parte-2/exercicio-73.py
@@ -1,13 +1,3 @@
-numeros = [0,0,1,1,2,2,3,3,4,4,4]
-
-numeros_filtrados = []
-
-for numero in numeros:
-    if(numero in numeros_filtrados):
-        exit 
-    else:
-        numeros_filtrados.append(numero)
-
-print('Numeros:', numeros)
-print('Numeros filtrados', numeros_filtrados)
-print('Tamanho da lista:', len(numeros_filtrados))
+numbers = [0,0,1,1,2,2,3,3,4,4,4]
+numbers = list(set(numbers))
+print(numbers)
