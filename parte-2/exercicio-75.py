550e2cfb53e12a35a8b89ce375640b17619d0939
Lucas Silva <lguilherme8@gmail.com>
2022-07-19 21:26:01 -0300

    edit: lógica nova e menos verbosa, Exercicio 75; style: mudança de código para inglẽs

--- parte-2/exercicio-75.py
+++ parte-2/exercicio-75.py
@@ -1,8 +1,9 @@
-def remove_da_lista(lista, numero):
-    for valor in lista:
-        if valor == numero:
-            lista.remove(valor)
+def remove_from_list(list, number):
+    while number in list:
+        list.remove(number)
+    print(len(list))
 
-    print(len(lista))
-
-remove_da_lista([1,2,3,4,5,6,7,5], 5)
+remove_from_list([1,2,3,4,5,6,7,5], 5)
+remove_from_list([10, 10, 10, 10, 10], 10)
+remove_from_list([10, 10, 10, 10, 10], 20)
+remove_from_list([], 1)
