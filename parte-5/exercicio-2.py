4f731a1e113af1962ddcfdf318e2f34b84e60404
Lucas Silva <lguilherme8@gmail.com>
2022-07-25 17:33:41 -0300

    fix: função corrigida de get para print, Exercicio-2, Parte-5

--- parte-5/exercicio-2.py
+++ parte-5/exercicio-2.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_all_paragraphs(url):
+def print_all_paragraphs(url):
     page = requests.get(url)
     print(BeautifulSoup(page.content).find_all('p'))
 
-get_all_paragraphs('https://www.github.com/slenderb13')
+print_all_paragraphs('https://www.github.com/slenderb13')
